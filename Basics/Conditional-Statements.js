b76fb00b31c9955988f65c60dec91f51520bf6fb
Sachin T P. <sachin.tp@excelindia.com>
2024-11-21 13:21:49 +0530

    Conditional statements

---
+++ Basics/Conditional-Statements.js
@@ -0,0 +1,20 @@
+// JavaScript allows you to make decisions in your code using if, else, and else if.
+
+// Basic if statement:
+let temperature = 30;
+if (temperature > 25) {
+  console.log("It's very hot outside!");
+} else {
+  console.log("It's very cool outside");
+}
+
+let age = 18;
+if (age < 18) {
+  console.log("You are minor!");
+} else if (age === 18) {
+  console.log("You just turned to 18!");
+} else if (age > 18) {
+  console.log("You are an Adult!");
+} else {
+  console.log("Invalid age!");
+}
